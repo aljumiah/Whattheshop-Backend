f5b0c08f6e1b5f88ffa90c6833b1afa182c0965a
anassu1400 <anassu1400@gmail.com>
2019-04-01 11:45:18 +0300

    create product and product images models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+class Product(models.Model):
+	name= models.CharField(max_length=50)
+	price= models.DecimalField(max_digits=6, decimal_places=2)
+	description = models.TextField()
+
+class ProductImages(models.Model):
+	image = models.ImageField()
+	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL, related_name='images')
