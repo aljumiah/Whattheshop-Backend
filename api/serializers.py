75a70bfb260bb64641b190127cb536bc2368def7
anassu1400 <anassu1400@gmail.com>
2019-04-01 22:39:56 +0300

    return token when registering

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Product, ProductImage , Cart , CartItem, Order, Profile
+from rest_framework_jwt.settings import api_settings
 
 class UserCreateSerializer(serializers.ModelSerializer):
 	password = serializers.CharField(write_only=True)
+	token = serializers.CharField(read_only=True)
 	class Meta:
 		model = User
-		fields = ['username', 'password']
+		fields = ['username', 'password', 'token']
 
 	def create(self, validated_data):
 		username = validated_data['username']
@@ -16,6 +18,10 @@ class UserCreateSerializer(serializers.ModelSerializer):
 		new_user.save()
 		profile = Profile(user=new_user)
 		profile.save()
+		jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+		jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+		payload = jwt_payload_handler(new_user)
+		validated_data['token'] = jwt_encode_handler(payload)
 		return validated_data
 
 class ProductImageSerializer(serializers.ModelSerializer):
