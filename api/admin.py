c9908ff792e4f8c92a133b3db0e3e409993927d9
sitahlatif <sitah27@hotmail.com>
2019-04-01 12:34:43 +0300

    cartmodel

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, ProductImages
+from .models import Product, ProductImages, Cart
 
 admin.site.register(Product)
 admin.site.register(ProductImages)
+admin.site.register(Cart)
--- api/models.py
+++ api/models.py
@@ -8,3 +8,9 @@ class Product(models.Model):
 class ProductImages(models.Model):
 	image = models.ImageField()
 	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL, related_name='images')
+
+class Cart(models.Model):
+	#Edit the model name
+	cart_Item = models.OneToOneField(Product, null=True, on_delete=models.SET_NULL, related_name='items')
+	sub_total= models.DecimalField(max_digits=6, decimal_places=2)
+	quentity= models.IntegerField()
