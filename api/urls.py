db502d49064e1dc950428503757b8fbc8151bbbb
anassu1400 <anassu1400@gmail.com>
2019-04-01 13:13:28 +0300

    serializer and view for product list

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserCreateAPIView
+from .views import (UserCreateAPIView, ProductListView, )
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('login/', obtain_jwt_token, name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('products/list/', ProductListView.as_view(), name='products_list')
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
-from rest_framework.generics import CreateAPIView
-from .serializers import UserCreateSerializer
+from rest_framework.generics import (CreateAPIView, ListAPIView, RetrieveAPIView, )
+from .serializers import (UserCreateSerializer, ProductListSerializer, )
+from .models import Product
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+class ProductListView(ListAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ProductListSerializer
