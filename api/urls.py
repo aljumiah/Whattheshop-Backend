f671c51ffb35c408641da8729851e96196b704f5
anassu1400 <anassu1400@gmail.com>
2019-04-01 17:17:04 +0300

    product detail and crud added

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
-from .views import (UserCreateAPIView, ProductListView, ProductDetailView)
+from .views import (
+UserCreateAPIView,
+ ProductListView, 
+ ProductImageAddView, ProductDetailView,
+ ProductUpdateView, 
+ ProductDeleteView, 
+ ProductCreateView,
+ ProductImageDeleteView,
+ ProductImageUpdateView,
+  )
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('login/', obtain_jwt_token, name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('products/list/', ProductListView.as_view(), name='products_list'),
-    path('products/<int:product_id>/detail/', ProductDetailView.as_view(), name='products_detail')
+    path('products/list/', ProductListView.as_view(), name='products-list'),
+
+    path('products/<int:product_id>/detail/', ProductDetailView.as_view(), name='products-detail'),
+    path('products/<int:product_id>/update/', ProductUpdateView.as_view(), name='products-update'),
+    path('products/<int:product_id>/delete/', ProductDeleteView.as_view(), name='products-delete'),
+    path('products/create/', ProductCreateView.as_view(), name='products-update'),
+    path('products/<int:product_id>/image/add/', ProductImageAddView.as_view(), name='product-image-add'),
+    path('products/<int:product_id>/image/<int:image_id>/update/', ProductImageUpdateView.as_view(), name='product-image-update'),
+    path('products/<int:product_id>/image/<int:image_id>/delete/', ProductImageDeleteView.as_view(), name='product-image-delete'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
-from rest_framework.generics import (CreateAPIView, ListAPIView, RetrieveAPIView, )
-from .serializers import (UserCreateSerializer, ProductListSerializer, ProductDetailSerializer, )
-from .models import Product
+from rest_framework.generics import (CreateAPIView, ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, CreateAPIView, DestroyAPIView, )
+from .serializers import (UserCreateSerializer, ProductListSerializer, ProductDetailSerializer, ProductCreateUpdateSerializer, ProductImageSerializer, )
+from .models import Product, ProductImage
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
@@ -14,3 +14,40 @@ class ProductDetailView(RetrieveAPIView):
 	serializer_class = ProductDetailSerializer
 	lookup_field = 'id'
 	lookup_url_kwarg = 'product_id'
+
+class ProductUpdateView(RetrieveUpdateAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ProductCreateUpdateSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'product_id'	
+
+class ProductCreateView(CreateAPIView):
+	serializer_class = ProductCreateUpdateSerializer
+
+class ProductDeleteView(DestroyAPIView):
+	queryset = Product.objects.all()
+	lookup_field = 'id'
+	lookup_url_kwarg = 'product_id'	
+
+class ProductImageAddView(CreateAPIView):
+	serializer_class = ProductImageSerializer
+	
+	def perform_create(self, serializer):
+		product = Product.objects.get(id=self.kwargs['product_id'])
+		serializer.save(product=product)
+
+class ProductImageUpdateView(RetrieveUpdateAPIView):
+	serializer_class = ProductImageSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'image_id'
+	def get_queryset(self):
+		images = ProductImage.objects.filter(product__id=self.kwargs['product_id'])
+		return images
+	
+class ProductImageDeleteView(DestroyAPIView):
+	lookup_field = 'id'
+	lookup_url_kwarg = 'image_id'
+	def get_queryset(self):
+		images = ProductImage.objects.filter(product__id=self.kwargs['product_id'])
+		return images
+
