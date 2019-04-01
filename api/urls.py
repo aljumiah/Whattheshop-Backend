a8d871fb09ca56882875861326308ebc1bcc34ce
Rasha Alharhi <rasha.alorabi@gmail.com>
2019-04-01 17:38:27 +0300

    cart cart item crud, orders crud

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
-from .views import (UserCreateAPIView, ProductListView, ProductDetailView)
+from .views import (
+	UserCreateAPIView, 
+	ProductListView, 
+	ProductDetailView,
+	CartListView,
+	CartItemUpdateView,
+	CartListDeleteView,
+	CartItemCreateView,
+	OrderCreateView)
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('login/', obtain_jwt_token, name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('products/list/', ProductListView.as_view(), name='products_list'),
-    path('products/<int:product_id>/detail/', ProductDetailView.as_view(), name='products_detail')
+    path('products/list/', ProductListView.as_view(), name='products-list'),
+    path('products/<int:product_id>/detail/', ProductDetailView.as_view(), name='products-detail'),
+    path('cart/', CartListView.as_view(), name='items'),
+    path('cart/<int:cart_id>/delete/',CartListDeleteView.as_view(), name='cart-delete'),
+    path('cart/items/<int:product_id>/add',CartItemCreateView.as_view(), name='cart-item-add'),
+    path('cart/items/<int:item_id>/update/',CartItemUpdateView.as_view(), name='items-update'),
+    path('cart/checkout/',OrderCreateView.as_view(), name='checkout'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,18 @@
-from rest_framework.generics import (CreateAPIView, ListAPIView, RetrieveAPIView, )
-from .serializers import (UserCreateSerializer, ProductListSerializer, ProductDetailSerializer, )
-from .models import Product
+from rest_framework.generics import (
+	CreateAPIView, 
+	ListAPIView, 
+	RetrieveAPIView,
+	RetrieveUpdateAPIView,
+	DestroyAPIView )
+from .serializers import (
+	UserCreateSerializer, 
+	ProductListSerializer, 
+	ProductDetailSerializer,
+	CartItemListSerializer,
+	CartListSerializer,
+	CartItemCreateUpdateSerializer,
+	OrderCreateSerializer )
+from .models import Product, CartItem, Cart, Profile
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
@@ -14,3 +26,36 @@ class ProductDetailView(RetrieveAPIView):
 	serializer_class = ProductDetailSerializer
 	lookup_field = 'id'
 	lookup_url_kwarg = 'product_id'
+
+class CartListView(ListAPIView):
+	queryset = Cart.objects.all()
+	serializer_class = CartListSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'item_id'
+
+class CartItemUpdateView(RetrieveUpdateAPIView):
+	queryset = CartItem.objects.all()
+	serializer_class = CartItemCreateUpdateSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'item_id'
+
+class CartItemCreateView(CreateAPIView):
+	serializer_class = CartItemCreateUpdateSerializer
+
+	def perform_create(self, serializer):	
+		product= Product.objects.get(id=self.kwargs['product_id'])
+		serializer.save(product=product)
+
+class CartListDeleteView(DestroyAPIView):
+	queryset = Cart.objects.all()
+	serializer_class = CartListSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'cart_id'
+
+class OrderCreateView(CreateAPIView):
+	serializer_class=OrderCreateSerializer
+
+	def perform_create(self, serializer):	
+		cart = Cart.objects.get(user=self.request.user)
+		profile = Profile.objects.get(user=self.request.user)
+		serializer.save(cart=cart, profile=profile)
