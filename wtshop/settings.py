2c5fd3e0a98cb80e403b5d63ea0fd60bc2ef67e9
anassu1400 <anassu1400@gmail.com>
2019-04-03 14:35:16 +0300

    cors origin whitelist

--- wtshop/settings.py
+++ wtshop/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
+    'corsheaders',
 
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',   
+)
 
 ROOT_URLCONF = 'wtshop.urls'
 
